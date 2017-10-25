59ec6a6ce261140d10de21e89d0aa465b7cd2bfd
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-25 16:37:14 -0200

    Correção

--- reflexao/src/reflexao/Reflexao.java
+++ reflexao/src/reflexao/Reflexao.java
@@ -5,19 +5,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Reflexao {
-	
-	public static List<String> getAtributos(Object o, String busca){
+
+	public static List<String> buscaStringEmAtributos(Object o, String buscada) {
 		try {
 			List<String> lista = new ArrayList<>();
 			Class<?> c = o.getClass();
-			for(Field f : c.getFields()) {
+			for (Field f : c.getFields()) {
 				Object value = f.get(o);
-				if(value.equals(busca)) {
-					lista.add(f.getName());
+				if (value != null) {
+					String strValue = value.toString();
+					if (strValue.contains(buscada)) {
+						lista.add(f.getName());
+					}
 				}
 			}
 			return lista;
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
