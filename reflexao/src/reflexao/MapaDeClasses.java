62d132c43d6635043b59e35dca80721c7e0f5de8
Intera-PC <Intera-PC@LAPTOP-MPVMO4BN.bs.br.bsch>
2017-10-26 09:49:45 -0200

    Retornar um mapa dos atributos e seus valores

--- reflexao/src/reflexao/MapaDeClasses.java
+++ reflexao/src/reflexao/MapaDeClasses.java
@@ -1,32 +1,44 @@
 package reflexao;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapaDeClasses {
-	
-	private Map<String,String> mapa;
 
-    public Class getClass(String chave) throws Exception{
-        String valor = mapa.get(chave);
-        if(valor != null){
-            return Class.forName(valor);
-        }else{
-            throw new RuntimeException("Chave inválida");
-        }
-    }
-    
-    public Object getObject(String classe) throws Exception {
-    	return getClass(classe).newInstance();
-    }
-    
-    public Object getObject(String chave, Object[] params) throws NoSuchMethodException, SecurityException, Exception {
-    	Class<?>[] tiposConstrutor = new Class<?>[params.length];
-        for(int i=0; i<tiposConstrutor.length; i++){
-            tiposConstrutor[i] = params[i].getClass();
-        }
-        Constructor<?> c = getClass(chave).getConstructor(tiposConstrutor); 
-        return c.newInstance(params);
-    }
+	private Map<String, String> mapa;
+
+	public Class getClass(String chave) throws Exception {
+		String valor = mapa.get(chave);
+		if (valor != null) {
+			return Class.forName(valor);
+		} else {
+			throw new RuntimeException("Chave inválida");
+		}
+	}
+
+	public Object getObject(String classe) throws Exception {
+		return getClass(classe).newInstance();
+	}
+
+	public Object getObject(String chave, Object[] params) throws NoSuchMethodException, SecurityException, Exception {
+		Class<?>[] tiposConstrutor = new Class<?>[params.length];
+		for (int i = 0; i < tiposConstrutor.length; i++) {
+			tiposConstrutor[i] = params[i].getClass();
+		}
+		Constructor<?> c = getClass(chave).getConstructor(tiposConstrutor);
+		return c.newInstance(params);
+	}
+
+	public static Map<String, Object> getFieldMap(Object obj) throws Exception {
+		Map<String, Object> map = new HashMap<>();
+		Class<?> c = obj.getClass();
+		for (Field f : c.getDeclaredFields()) {
+			f.setAccessible(true);
+			map.put(f.getName(), f.get(obj));
+		}
+		return map;
+	}
 
 }
